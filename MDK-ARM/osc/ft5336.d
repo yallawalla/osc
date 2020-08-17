b7f9d1552653fce3089e644090e88ec95156c43d
MOCNIK <matjaz.mocnik@fotona.com>
2020-08-17 16:26:33 +0200

    ignore objects..

--- MDK-ARM/osc/ft5336.d
+++
@@ -1,4 +0,0 @@
-.\osc\ft5336.o: ..\Components\ft5336\ft5336.c
-.\osc\ft5336.o: ..\Components\ft5336\ft5336.h
-.\osc\ft5336.o: ..\Components\ft5336\../Common/ts.h
-.\osc\ft5336.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
